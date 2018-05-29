57b37bc6b493047997b11245b5bf6310854dc07a
Matt <[mailprogramisty@gmail.com]>
2018-05-29 14:21:23 +0200

    v1.0

--- 8 Ball/8 Ball/ViewController.swift
+++ 8 Ball/8 Ball/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    var randomBallNumber : Int = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        randomBallUpdate()
+
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func randomBallUpdate() {
+        randomBallNumber = Int(arc4random_uniform(UInt32(ballArray.count)))
+        imageView.image = UIImage (named: "ball\(randomBallNumber)")
+    }
 
+    @IBAction func askButtonPressed(_ sender: Any) {
+        randomBallUpdate()
+    }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        randomBallUpdate()
+    }
 }
+
 
